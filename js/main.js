ea2b8d2db12207d19c90dddb41d9e4e7a5176d51
Ruben Schmidmeister <ruben.schmidmeister@icloud.com>
2016-06-14 19:01:43 +0200

    add fetch polyfill, support for async/await

--- js/main.js
+++ js/main.js
@@ -1,1 +1,10 @@
-console.log('hello world')
+import 'whatwg-fetch'
+
+(async () => {
+  
+  const resp = await fetch('https://httpbin.org/get')
+  const data = await resp.json()
+
+  console.log(data)
+  
+})()
