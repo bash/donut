0fa7d1648ac91081bfaa5fcced310ae1b5a162f3
Ruben Schmidmeister <ruben.schmidmeister@icloud.com>
2016-07-26 19:57:57 +0200

    update js sample

--- js/donut.js
+++ js/donut.js
@@ -1,3 +1,9 @@
-fetch('https://httpbin.org/get')
+/**
+ * (c) 2016 Ruben Schmidmeister
+ */
+
+const url = 'https://httpbin.org/get'
+
+fetch(url)
   .then((resp) => resp.json())
   .then((data) => console.log(data))
